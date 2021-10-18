013b72460880f54621e5c11b1fb3db17a697627c
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2021-10-18 13:57:20 -0400

    Added platforms

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,7 +5,28 @@ export default class GameScene extends Phaser.Scene {
     super("game-scene");
   }
 
-  preload() {}
+  preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
 
-  create() {}
+    this.load.spritesheet("dude", "assets/dude.png", { frameWidth: 32, frameHeight: 48 });
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+    this.add.image(400, 300, "star");
+    this.createPlatforms();
+  }
+
+  createPlatforms() {
+    const platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 220, "ground");
+  }
 }
